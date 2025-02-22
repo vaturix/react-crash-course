80e11e76fbbd6c0c6d41a78277fcc7becd7ab548
İsa Öztürk <isa.ozturk@csb.gov.tr>
2025-02-22 18:14:36 +0300

    Add post creation functionality and display posts in PostsList component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,16 +6,29 @@ import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts] );
+  }
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author="Manuel" body="Check out the full course!" />
-      </ul>
+      {posts.length > 0 && (
+ <ul className={classes.posts}>
+ {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
+ </ul>
+      )}
+     {posts.length === 0 && (
+      <div style={{textAlign: 'center', color: 'white'}}>
+        <h2>There are no posts yet.</h2>
+        <p>Start adding some!</p>
+      </div>
+     )}
     </>
   );
 }
